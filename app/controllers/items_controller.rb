6d2e2c9ff371e60e5c72e7bfb4ebfa4784469365
Hiro0608 <h.satou1991@gmail.com>
2022-08-13 16:18:18 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,10 +18,28 @@ before_action :authenticate_user!, except: [:index, :show]
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :price, :introduction, :category_id, :status_id, :trading_price_id, :trading_area_id, :trading_days_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :price, :introduction, :category_id, :status_id, :trading_price_id, :trading_area_id, :trading_day_id).merge(user_id: current_user.id)
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -42,12 +42,12 @@ ActiveRecord::Schema.define(version: 2022_08_08_053749) do
     t.string "name", null: false
     t.integer "price", null: false
     t.text "introduction", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.integer "category_id", null: false
     t.integer "status_id", null: false
     t.integer "trading_price_id", null: false
     t.integer "trading_area_id", null: false
-    t.integer "trading_days_id", null: false
+    t.integer "trading_day_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
@@ -92,4 +92,5 @@ ActiveRecord::Schema.define(version: 2022_08_08_053749) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
