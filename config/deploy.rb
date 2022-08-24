c75c10dc6e3de188abf860839c7001e98b17cf9c
Hiro0608 <h.satou1991@gmail.com>
2022-08-24 20:36:28 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.17.1'
 set :application, 'furima-38192'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'Hiro0608/furima-38192.git'
+set :repo_url,  'git@github.com:Hiro0608/furima-38192.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
